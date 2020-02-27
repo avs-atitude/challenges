07509cb6f7aa95a72d1032701587eba621a18adf
Alexandre <alexandre.geek.developer@gmail.com>
2020-02-27 05:49:40 -0300

    Cassino
    * Add Describe in test case
    * Upgrade in description test

--- src/casino/casino.test.js
+++ src/casino/casino.test.js
@@ -1,35 +1,43 @@
 const { solution } = require('./casino');
 
-test('shoud be definied', () => {
-    expect(solution).toBeDefined();
-});
-
-
-test('shoud be equal', () => {
-    const tests = [
-        {
-            n: 8,
-            k: 0,
-            expected: 7
-        },
-    
-        {
-            n: 18,
-            k: 2,
-            expected: 6
-        },
-    
-        {
-            n: 10,
-            k: 10,
-            expected: 4
-        },
-    ];
-
-
-    tests.forEach(test => {
-        let response = solution(test.n, test.k)
-        expect(response).toEqual(test.expected);
+describe('CASSINO', () => {
+    test("shoud be definied function 'solution'", () => {
+        expect(solution).toBeDefined();
     });
     
-})
+    
+    test("the return of the 'solution' must equal the value of the response", () => {
+        const tests = [
+            {
+                n: 8,
+                k: 0,
+                expected: 7
+            },
+        
+            {
+                n: 18,
+                k: 2,
+                expected: 6
+            },
+        
+            {
+                n: 10,
+                k: 10,
+                expected: 4
+            },
+    
+            {
+                n: 30,
+                k: 4,
+                expected: 7
+            },
+        ];
+    
+    
+        tests.forEach(test => {
+            let response = solution(test.n, test.k)
+            expect(response).toEqual(test.expected);
+        });
+        
+    });
+});
